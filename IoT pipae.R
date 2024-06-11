f51f96d07c78d73fcb90dc5a60140ca88ed2dd9d
PedrMartins <62296977+PedrMartins@users.noreply.github.com>
2024-06-11 00:24:07 -0300

    Update IoT pipae.R

--- IoT pipae.R
+++ IoT pipae.R
@@ -2,34 +2,47 @@
 ###############################################################################
 ##############################ELEGÂNCIA EM BAIXAR PACOTES#######################
 
-pkg <- c('googlesheets4',"lubridate", "timetk","dplyr")
+pkg <- c('googlesheets4',"lubridate", "timetk","dplyr","corrplot")
 pkg <- pkg[!pkg%in%installed.packages()]
 install.packages ("pkg")
 #########################
-install.packages("dplyr")
-install.packages("corrplot")
-install.packages("timetk")
 library (googlesheets4)
 library(lubridate)
 library(timetk)
 library(dplyr)
-library(corrplot)
+library(tidyr)
+#library(corrplot)
 
 #Read google sheets data into R
-pi_d <- read_sheet("https://docs.google.com/spreadsheets/d/12JiclFJ9luvW9JPee77TJh1zCGnaPTcfv_RN6pl7hRk/edit?usp=sharing")	
+pi_raw <- read_sheet("https://docs.google.com/spreadsheets/d/12JiclFJ9luvW9JPee77TJh1zCGnaPTcfv_RN6pl7hRk/edit?usp=sharing")	
 
-pi_d  
+
+########################################################
+#####################Tratando dados##################
+
+pi_d <- pi_raw 
   head (pi_d)
 pi_d$D_M_A <- lubridate::as_date (pi_d$Data)
-names (pi_d)
-plot (CO2~Temperatura, type="n",data=pi_d)
+pi_d <- pi_d [,-7]
+pi_d<- separate(pi_d, 
+    col = Hora,
+    into = c("data_false", "h"), 
+    sep = " "
+  )
+pi_d <- pi_d [,-7]
 
-points(CO2~Temperatura,
-  col="gray4" ,pch =19,
-  data=pi_d)
+pi_d
+
+#teste
+hms (c("00:29:06", "00:29:17",
+	 "00:29:17", "00:29:26", 
+	"00:29:36", "00:29:46"), tz="UTC"	
+	)
 
 ##############################################
-#Média ṕor dia
+#################Análises ####################
+
+
 med_p = pi_d |>
   summarize_by_time (.date_var=D_M_A,
                      by="day", 
@@ -38,19 +51,10 @@ med_p = pi_d |>
                      medpre=mean(Pressão)
                     )
 
+
 l_p=lm (mco2_p$med~mco2_p$D_M_A)
 
-
-###Estrutura gráfica
-
-plot (medtem~D_M_A ,type="n", data= med_p)
-lines (medtem~D_M_A, data = med_p) #plota linhas 
-abline (l_p) #plot modelo linear
-med_p <- med_p [-15,]
-c_p=cor(as.data.frame(med_p[c(3:5)]))
-corrplot (c_p)    
-
-##############################################
+ls()
 
 ##############################################
 ##############################################
@@ -74,11 +78,28 @@ med_ph = pi_d |>
   )
 med_ph
 
+############Estrutura gráfica########################
+
+plot (medtem~D_M_A ,type="n", data= med_p)
+lines (medtem~D_M_A, data = med_p) #plota linhas 
+abline (l_p) #plot modelo linear
+med_p <- med_p [-15,]
+c_p=cor(as.data.frame(med_p[c(3:5)]))
+corrplot (c_p)    
+
+
+plot (CO2~Temperatura, type="n",data=pi_d)
+
+points(CO2~Temperatura,
+  col="gray4" ,pch =19,
+  data=pi_d)
+
+
 plot (medco2~medtem ,data=med_ph)
 
+##################################################################
 
-ls()
+
 #Dividir os dados, pode ser importante colocar médias diárias (vai reduzir os dados)
 #dividir em dia e noite pode ser uma boa idéia
 #limpar outliers, mas devo ver os dados primeiro / preciso fazer uma análise exploratória
-
