5435afcb9ec503aff0e162582fcd9da3ff312642
PedrMartins <62296977+PedrMartins@users.noreply.github.com>
2024-06-11 15:51:38 -0300

    Update IoT pipae.R

--- IoT pipae.R
+++ IoT pipae.R
@@ -2,17 +2,19 @@
 ###############################################################################
 ##############################ELEGÂNCIA EM BAIXAR PACOTES#######################
 
-pkg <- c('googlesheets4',"lubridate", "timetk","dplyr","corrplot")
-pkg <- pkg[!pkg%in%installed.packages()]
-install.packages ("pkg")
+pkg <- c('googlesheets4',"lubridate", "timetk","dplyr","corrplot", "tidyr")
+pkg <- pkg[!pkg%in%installed.packages()] 
+install.packages (pkg)
 #########################
 library (googlesheets4)
 library(lubridate)
 library(timetk)
 library(dplyr)
 library(tidyr)
-#library(corrplot)
+library(corrplot)
 
+
+########################################################
 #Read google sheets data into R
 pi_raw <- read_sheet("https://docs.google.com/spreadsheets/d/12JiclFJ9luvW9JPee77TJh1zCGnaPTcfv_RN6pl7hRk/edit?usp=sharing")	
 
@@ -21,85 +23,76 @@ pi_raw <- read_sheet("https://docs.google.com/spreadsheets/d/12JiclFJ9luvW9JPee7
 #####################Tratando dados##################
 
 pi_d <- pi_raw 
-  head (pi_d)
-pi_d$D_M_A <- lubridate::as_date (pi_d$Data)
-pi_d <- pi_d [,-7]
-pi_d<- separate(pi_d, 
-    col = Hora,
-    into = c("data_false", "h"), 
-    sep = " "
-  )
-pi_d <- pi_d [,-7]
+pi_d <- pi_d |> 
+  mutate(Yf=year(pi_d$Hora),
+         Mf=month(pi_d$Hora),
+         Df=day(pi_d$Hora),
+         H=hour(pi_d$Hora),
+         M=minute(pi_d$Hora),
+         S=second(pi_d$Hora))
 
-pi_d
+##por hora
+pi_d <- pi_d [,-c(5,6,9:11)]
 
-#teste
-hms (c("00:29:06", "00:29:17",
-	 "00:29:17", "00:29:26", 
-	"00:29:36", "00:29:46"), tz="UTC"	
-	)
 
-##############################################
+pi_d |> 
+  head()
+pi_d |> 
+  srt()
+
+
+############################################
 #################Análises ####################
 
 
-med_p = pi_d |>
-  summarize_by_time (.date_var=D_M_A,
+med_pd = pi_d |>
+  summarize_by_time (.date_var=Data,
                      by="day", 
                      medco2=mean(CO2),
                      medtem=mean(Temperatura),
                      medpre=mean(Pressão)
-                    )
-
-
-l_p=lm (mco2_p$med~mco2_p$D_M_A)
-
-ls()
-
-##############################################
-##############################################
-
-#arrumar hora
-#trabalhoso
-pi_d$H <- lubridate::as_date (pi_d$Hora, tz=NULL)
-pi_d$H <- ymd_h(pi_d$H)
-pi_d$H <- round_date(pi_d$Hora, unit ="minute")
-
-plot (CO2~H, data = pi_d)
-#tem que fazer a média por hora <--
-
-
-med_ph = pi_d |>
-  summarize_by_time (.date_var=Hora,
-                     by="hours", 
-                     medco2=mean(CO2),
-                     medtem=mean(Temperatura),
-                     medpre=mean(Pressão)
   )
-med_ph
+med_pd
 
+l_p=lm (medco2~medpre, data= med_p)
+
+med_ph= pi_d|> 
+  group_by(H)|>
+  summarise(medco2=mean(CO2),
+            medtem=mean(Temperatura),
+            medpre=mean(Pressão))
+med_ph
 ############Estrutura gráfica########################
 
-plot (medtem~D_M_A ,type="n", data= med_p)
-lines (medtem~D_M_A, data = med_p) #plota linhas 
+plot (medco2~medpre ,type="p", data= med_pd)
+lines (medtem~medpre, data = med_pd) #plota linhas 
 abline (l_p) #plot modelo linear
-med_p <- med_p [-15,]
+med_p <- med_p [-15,] #retirando NA
 c_p=cor(as.data.frame(med_p[c(3:5)]))
 corrplot (c_p)    
 
+plot (medco2~H, data=med_ph, type="p")
+lines (medco2~H, data=med_ph) #plota linhas 
+lines(medtem~H, data=med_ph)
+lines(medpre~H, data=med_ph)
 
-plot (CO2~Temperatura, type="n",data=pi_d)
+plot (CO2~Temperaturamed_phplot (CO2~Temperatura, type="n",data=pi_d)
 
 points(CO2~Temperatura,
-  col="gray4" ,pch =19,
-  data=pi_d)
+       col="gray70" ,pch =19,
+       data=pi_d)
 
 
 plot (medco2~medtem ,data=med_ph)
 
-##################################################################
+#######################################################
 
+x <- y <- ymd_hms("2012-03-26 10:10:00", tz = "America/Sao_Paulo")
+tz(x)
 
+tz(y) <- "America/Sao_Paulo"
+
+x-y
 #Dividir os dados, pode ser importante colocar médias diárias (vai reduzir os dados)
 #dividir em dia e noite pode ser uma boa idéia
 #limpar outliers, mas devo ver os dados primeiro / preciso fazer uma análise exploratória
